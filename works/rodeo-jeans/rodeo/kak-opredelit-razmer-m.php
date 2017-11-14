bfd03ed5eff4b9bba72904c4cb120e29afcd7956
alex lyulin <kutuzov24qwerty@gmail.com>
2017-11-14 19:34:10 +0700

    test

---
+++ works/rodeo-jeans/rodeo/kak-opredelit-razmer-m.php
@@ -0,0 +1,252 @@
+<?php
+
+include('tmp/header.php');
+?>
+
+
+<div class="section content">
+      <div class="container">
+		<div class="row">
+
+			<div class="col-md-12">
+				
+				
+				
+	<!--  WORK AREA -->
+
+	<div class="dtrm">
+
+		<div class="wrapper__dtrm">
+			<div class="dtrm__header">
+				<a href="#"><img class="close-block" src="images/pages/exit.svg" alt=""></a>
+				<span class="dtrm__header-bold">
+					Мужчинам: Определите свой размер
+				</span>
+				<div class="dtrm__header-title">
+					<a href="#" class="dtrm__header-title--active">
+						Размерные таблицы 
+					</a>
+					<a href="#" class="dtrm__header-title--noactive">
+						/ Замеры для определения размера
+					</a>
+				</div>
+			</div>
+
+
+			<div class="dtrm__sizes">
+
+				<div class="dtrm__sizes-title">
+					Таблица №1: Размеры мужских джинсов 
+				</div>
+				<div class="dtrm__sizes-details">
+					<span class="dtrm__sizes-details-span"> Подробнее в разделе </span>
+					<a class="dtrm__sizes-details--link" href="#"> Как определить размер </a>
+				</div>
+
+				<!--first table-->
+				<div class="table-responsive">
+					<table class="table ">
+						<tr>
+							<th class="table__title-main"  colspan="2">Обхват талии, см</th>
+							<td class="table__title">70-73</td>
+							<td class="table__title">73-75</td>
+							<td class="table__title">75-78</td>
+							<td class="table__title">78-80</td>
+							<td class="table__title">80-83</td>
+							<td class="table__title">83-85</td>
+							<td class="table__title">86-88</td>
+							<td class="table__title">88-89</td>
+							<td class="table__title">89-92</td>
+							<td class="table__title">93-95</td>
+							<td class="table__title">95-98</td>
+							<td class="table__title">98-100</td>
+							<td class="table__title">100-103</td>
+							<td class="table__title">103-106</td>
+						</tr>
+						<tr>
+							<th class="table__title">Рост, см</th>
+							<th class="table__title">Дюймы</th>
+							<td class="table__title">W28</td>
+							<td class="table__title">W29</td>
+							<td class="table__title">W30</td>
+							<td class="table__title">W31</td>
+							<td class="table__title">W32</td>
+							<td class="table__title">W33</td>
+							<td class="table__title">W34</td>
+							<td class="table__title">W35</td>
+							<td class="table__title">W36</td>
+							<td class="table__title">W38</td>
+							<td class="table__title">W40</td>
+							<td class="table__title">W42</td>
+							<td class="table__title">W44</td>
+							<td class="table__title">W46</td>
+						</tr>
+						<tr>
+							<td class="table__title">165-177</td>
+							<td class="table__title">L33*</td>
+							<td class="table__cell">28/32</td>
+							<td class="table__cell">29/32</td>
+							<td class="table__cell">30/32</td>
+							<td class="table__cell">31/32</td>
+							<td class="table__cell">32/32</td>
+							<td class="table__cell">33/32</td>
+							<td class="table__cell">34/32</td>
+							<td class="table__cell">35/32</td>
+							<td class="table__cell">36/32</td>
+							<td class="table__cell">38/32</td>
+							<td class="table__cell">40/32</td>
+							<td class="table__cell">42/32</td>
+							<td class="table__cell">44/32</td>
+							<td class="table__cell">46/32</td>
+						</tr>
+						<tr>
+							<td class="table__title">178-185</td>
+							<td class="table__title">L34*</td>
+							<td class="table__cell">28/34</td>
+							<td class="table__cell">29/34</td>
+							<td class="table__cell">30/34</td>
+							<td class="table__cell">31/34</td>
+							<td class="table__cell">32/34</td>
+							<td class="table__cell">33/34</td>
+							<td class="table__cell">34/34</td>
+							<td class="table__cell">35/34</td>
+							<td class="table__cell">36/34</td>
+							<td class="table__cell">38/34</td>
+							<td class="table__cell">40/34</td>
+							<td class="table__cell">42/34</td>
+							<td class="table__cell">44/34</td>
+							<td class="table__cell">46/34</td>
+						</tr>
+						<tr>
+							<td class="table__title">186-192</td>
+							<td class="table__title">L36*</td>
+							<td class="table__cell">28/36</td>
+							<td class="table__cell">29/36</td>
+							<td class="table__cell">30/36</td>
+							<td class="table__cell">31/36</td>
+							<td class="table__cell">32/36</td>
+							<td class="table__cell">33/36</td>
+							<td class="table__cell">34/36</td>
+							<td class="table__cell">35/36</td>
+							<td class="table__cell">36/36</td>
+							<td class="table__cell">38/36</td>
+							<td class="table__cell">40/36</td>
+							<td class="table__cell">42/36</td>
+							<td class="table__cell">44/36</td>
+							<td class="table__cell">46/36</td>
+						</tr>
+						<tr>
+							<td class="table__title">>193</td>
+							<td class="table__title">L38*</td>
+							<td class="table__cell">28/38</td>
+							<td class="table__cell">29/38</td>
+							<td class="table__cell">30/38</td>
+							<td class="table__cell">31/38</td>
+							<td class="table__cell">32/38</td>
+							<td class="table__cell">33/38</td>
+							<td class="table__cell">34/38</td>
+							<td class="table__cell">35/38</td>
+							<td class="table__cell">36/38</td>
+							<td class="table__cell">38/38</td>
+							<td class="table__cell">40/38</td>
+							<td class="table__cell">42/38</td>
+							<td class="table__cell">44/38</td>
+							<td class="table__cell">46/38</td>
+						</tr>
+					</table>
+				</div>
+				
+				<p class="dtrm__sizes-descr">
+					<span class="dtrm__sizes-descrDark" >*Мужские джинсы с 32 ростом бывают редко, обычно джинсы шьют 34 роста, бывают джинсы для высоких людей с ростом 36 и 38.</span>
+					<br>
+					<span class="dtrm__sizes-descrLight">**35 размер в мужских джинсах бывает крайне редко, обычно после 34 сразу идет 36 размер.
+					</span>
+					<br> <br>
+					<span class="dtrm__sizes-descrDark" >!Ваш размер может быть больше, либо меньше размера, определенного по таблице “Размеры мужских джинсов”.</span> 
+					<br> <br>
+
+					<span class="dtrm__sizes-descrLight">Это возникает вследствие индивидуальных пропорций фигуры, в том числе присутствия живота, различной эластичности ткани, предопочтений 
+					свободного или более плотного прилегания.</span> <br><br>
+
+					<span class="dtrm__sizes-descrDark--last">Таким образом, для определения размера нужно учитывать размер аналогичной одежды, которую Вы носили ранее.</span>
+				</p>
+			</div>
+
+			<div class="dtrm__sizes-jeans">
+				<div class="dtrm__sizes-title">
+					Таблица №2: Размеры мужской плечевой одежды
+				</div>
+
+				<div class="table-responsive">
+					<table class="table ">
+						<tr>
+							<th class="table__title-main"  colspan="2">Обхват груди, см</th>
+							<td class="table__title">84-90</td>
+							<td class="table__title">90-93</td>
+							<td class="table__title">94-97</td>
+							<td class="table__title">98-101</td>
+							<td class="table__title">102-105</td>
+							<td class="table__title">106-109</td>
+							<td class="table__title">110-113</td>
+							<td class="table__title">114-117</td>
+							<td class="table__title">118-122</td>
+						</tr>
+						<tr>
+							<th class="table__title-main"  colspan="2">Российский размер</th>
+							<td class="table__cell">44</td>
+							<td class="table__cell">46</td>
+							<td class="table__cell">48</td>
+							<td class="table__cell">50</td>
+							<td class="table__cell">52</td>
+							<td class="table__cell">54</td>
+							<td class="table__cell">56</td>
+							<td class="table__cell">58</td>
+							<td class="table__cell">60</td>
+						</tr>
+						<tr>
+							<th class="table__title-main"  colspan="2">Международный размер</th>
+							<td class="table__cell">S</td>
+							<td class="table__cell">M</td>
+							<td class="table__cell">L</td>
+							<td class="table__cell">XL</td>
+							<td class="table__cell">XXL</td>
+							<td class="table__cell">3XL</td>
+							<td class="table__cell">4XL</td>
+							<td class="table__cell">5XL</td>
+							<td class="table__cell">6XL</td>
+						</tr>
+					</table>
+				</div>
+				<p>
+					<span class="dtrm__sizes-jeans--descrNormal">
+						! Ваш размер может быть больше или меньше размера, определенного по таблице «Размеры мужской плечевой одежды».
+					</span>
+					<span class="dtrm__sizes-jeans--descrNormal">Это возникает вследствие индивидуальных особенностей фигуры, в том числе ширины плеч, наличия живота, предпочтений свободного или более плотного прилегания, эластичности ткани.</span> 
+					<span class="dtrm__sizes-jeans--descrNormal">Таким образом, для определения размера нужно учитывать размер аналогичной одежды, которую Вы носили ранее.</span> 
+				</p>
+				
+			</div>
+
+
+		</div>
+	</div>
+
+
+	<!-- END  WORK AREA -->
+
+
+
+			</div>
+			
+            
+		</div>
+	</div>
+</div> 
+
+
+
+
+<?php
+
+include('tmp/footer.php');
+?>
