1ed96766f4ba1cd69a78111c6942c4644cfcf7c0
lyaksay <kutuzov24qwerty@gmail.com>
2019-11-26 14:18:32 +0300

    max-dr

--- works/max/js/main.js
+++ works/max/js/main.js
@@ -3,9 +3,9 @@
 	}
 	$("#angels").hover(function () { 
 		$(this).css({
-			top: getRandomInt(0, 750)+"px",
-			left: getRandomInt(0, 750)+"px",
-			right: getRandomInt(0, 800)+"px",
-			bottom: getRandomInt(0, 250)+"px"
+			top: getRandomInt(0, 5750)+"px",
+			left: getRandomInt(0, 5750)+"px",
+			right: getRandomInt(0, 5800)+"px",
+			bottom: getRandomInt(0, 5800)+"px"
 		});
 	}, function () { });
